64ff4f45fd1e1cc4f9c9ca4fcf5a328978a8cc20
Katy Daviscourt <katy.daviscourt@gmail.com>
2017-01-27 11:29:42 -0800

    start new branch to try to fix problems from early on

--- js/scripts.js
+++ js/scripts.js
@@ -2,38 +2,38 @@
 var sizeArray = ['Small (10")', 'Medium (12")', 'Large (14")'];
 var toppingsArray = ["Pepperoni", "Pineapple", "Diced tomatoes", "Pesto", "Basil", "Extra cheese", "Olives"];
 
-var Pizza = function(size, toppings) {
+var Pizza = function(size, toppings, price) {
   this.selectedSize = size;
   this.selectedToppings = toppings;
   this.price = 12;
 };
 
-Pizza.prototype.sizePrice = function() {
-  if (this.selectedSize === sizeArray[0]) {
-    this.price += 0;
-  } else if (this.selectedSize === sizeArray[1]) {
-    this.price += 2;
-  } else if (this.selectedSize === sizeArray[2]) {
-    this.price += 4;
-  }
-};
+// Pizza.prototype.sizePrice = function() {
+//   if (this.selectedSize === sizeArray[0]) {
+//     this.price += 0;
+//   } else if (this.selectedSize === sizeArray[1]) {
+//     this.price += 2;
+//   } else if (this.selectedSize === sizeArray[2]) {
+//     this.price += 4;
+//   }
+// };
 
 //CAN I DO THIS AS A FOREACH LOOP INSTEAD OF IF STATEMENTS??
-Pizza.prototype.toppingsPrice = function() {
-  if (numberOfToppingsSelected === 0) {
-  } else if (numberOfToppingsSelected === 1) {
-    this.price +=2;
-  } else if (numberOfToppingsSelected === 2) {
-    this.price +=4;
-  } else if (numberOfToppingsSelected === 3) {
-    this.price +=6;
-  } else if (numberOfToppingsSelected === 4) {
-    this.price +=8;
-  } else {
-    this.price +=10;
-  }
-
-}
+// Pizza.prototype.toppingsPrice = function() {
+//   if (numberOfToppingsSelected === 0) {
+//   } else if (numberOfToppingsSelected === 1) {
+//     this.price +=2;
+//   } else if (numberOfToppingsSelected === 2) {
+//     this.price +=4;
+//   } else if (numberOfToppingsSelected === 3) {
+//     this.price +=6;
+//   } else if (numberOfToppingsSelected === 4) {
+//     this.price +=8;
+//   } else {
+//     this.price +=10;
+//   }
+//
+// }
 
 
 
@@ -41,11 +41,14 @@ Pizza.prototype.toppingsPrice = function() {
 //front-end logic
 $(document).ready(function(){
   var inputtedSize = $("select#size").val();
-  var numberOfToppingsSelected = $('input:checkbox:checked').length;
+  var inputtedToppings = $("input:checkbox:checked").val();
+  // var numberOfToppingsSelected = $('input:checkbox:checked').length;
+  var calculatedPrice = this.price;
+  var myPizza = new Pizza (inputtedSize, inputtedToppings, calculatedPrice);
 
-  myPizza.sizePrice();
-  myPizza.toppingsPrice();
-  
+  // myPizza.sizePrice();
+  // myPizza.toppingsPrice();
+
   $("form#order-form").submit(function(event){
       event.preventDefault();
 
